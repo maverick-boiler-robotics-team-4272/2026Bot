ab04ebf63c9bf6543b227fe2a8fae31810c4566a
DrRobbie <menon.elliott@gmail.com>
2026-03-03 12:01:05 -0600

    Add pid through trench

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -9,12 +9,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import java.util.ArrayList;
 
 public class FieldConstants {
-  public static final double HUB_HEIGHT_M = 1.83; // There may be a use case, but not at the moment
-  public static final double FUEL_MAX_HEIGHT_M = 3; // Ehh, we shall see...
-  public static final double ROBOT_MASS_KG = 68.4;
-  public static final double FIELD_LENGTH_M = 16.54;
-  public static final double FIELD_WIDTH_M = 8.07;
-  public static final SendableChooser<String> SIDE_CHOOSER = new SendableChooser<>();
+    public static final double HUB_HEIGHT_M = 1.83; // There may be a use case, but not at the moment
+    public static final double FUEL_MAX_HEIGHT_M = 3; // Ehh, we shall see...
+    public static final double ROBOT_MASS_KG = 68.4;
+    public static final double FIELD_LENGTH_M = 16.54;
+    public static final double FIELD_WIDTH_M = 8.07;
+    public static final SendableChooser<String> SIDE_CHOOSER = new SendableChooser<>();
 
     public static final Pose2d CLIMB_POSE_LEFT = new Pose2d(15.469, 3.297, Rotation2d.fromDegrees(90));
     public static final Pose2d CLIMB_POSE_RIGHT = new Pose2d(15.461, 5.357, Rotation2d.fromDegrees(-90));
@@ -24,22 +24,19 @@ public class FieldConstants {
     public static final Translation2d HUB_LOCATION = new Translation2d(
             !isRedSide() ? 4.75 : FIELD_LENGTH_M - 4.75, 4.035); // yup, I added the extra distance :)
 
-    public static final Pose2d TRENCH_LEFT_OUT_APPROACH = new Pose2d(13.265, 0.697, Rotation2d.kZero);
+    public static final Pose2d TRENCH_LEFT_OUT_APPROACH = new Pose2d(13.265, 0.657, Rotation2d.kZero);
 
     public static final Pose2d TRENCH_LEFT_OUT = new Pose2d(12.568, 0.657, Rotation2d.kZero);
 
-    public static final Pose2d TRENCH_LEFT_IN = new Pose2d(11.210, 0.610, Rotation2d.kZero);
+    public static final Pose2d TRENCH_LEFT_IN = new Pose2d(11.210, 0.657, Rotation2d.kZero);
 
-    public static final Pose2d TRENCH_LEFT_IN_APPROACH = new Pose2d(10.683, 0.664, Rotation2d.kZero);
+    public static final Pose2d TRENCH_LEFT_IN_APPROACH = new Pose2d(10.4, 0.657, Rotation2d.kZero);
 
-    public static final Pose2d TRENCH_RIGHT_OUT_APPROACH = new Pose2d(isRedSide() ? 7.4125 : FIELD_WIDTH_M - 7.4125,
-            isRedSide() ? FIELD_LENGTH_M - 3.03 : 3.03, Rotation2d.fromDegrees(180));
-    public static final Pose2d TRENCH_RIGHT_OUT = new Pose2d(isRedSide() ? 7.4125 : FIELD_WIDTH_M - 7.4125,
-            isRedSide() ? FIELD_LENGTH_M - 4.03 : 4.03, Rotation2d.fromDegrees(180));
-    public static final Pose2d TRENCH_RIGHT_IN = new Pose2d(isRedSide() ? 7.4125 : FIELD_WIDTH_M - 7.4125,
-            isRedSide() ? FIELD_LENGTH_M - 5.128 : 5.128, Rotation2d.fromDegrees(180));
-    public static final Pose2d TRENCH_RIGHT_IN_APPROACH = new Pose2d(isRedSide() ? 7.4125 : FIELD_WIDTH_M - 7.4125,
-            isRedSide() ? FIELD_LENGTH_M - 6.128 : 6.128, Rotation2d.fromDegrees(180));
+    public static final Pose2d TRENCH_RIGHT_OUT_APPROACH = new Pose2d(13.265, FIELD_WIDTH_M - 0.657,
+            Rotation2d.kZero);
+    public static final Pose2d TRENCH_RIGHT_OUT = new Pose2d(12.568, FIELD_WIDTH_M - 0.657, Rotation2d.kZero);
+    public static final Pose2d TRENCH_RIGHT_IN = new Pose2d(11.210, FIELD_WIDTH_M - 0.657, Rotation2d.kZero);
+    public static final Pose2d TRENCH_RIGHT_IN_APPROACH = new Pose2d(10.4, FIELD_WIDTH_M - 0.657, Rotation2d.kZero);
 
     public static final Pose2d[] LEFT_OUT = { TRENCH_LEFT_OUT_APPROACH, TRENCH_LEFT_OUT, TRENCH_LEFT_IN,
             TRENCH_LEFT_IN_APPROACH };
@@ -56,17 +53,17 @@ public class FieldConstants {
         CLIMB_POSES.add(CLIMB_POSE_RIGHT);
         CLIMB_POSES.add(CLIMB_POSE_LEFT);
 
-        TRENCH_POSES.add(TRENCH_LEFT_OUT_APPROACH);
-        TRENCH_POSES.add(TRENCH_LEFT_IN_APPROACH);
         TRENCH_POSES.add(TRENCH_RIGHT_OUT_APPROACH);
         TRENCH_POSES.add(TRENCH_RIGHT_IN_APPROACH);
+        TRENCH_POSES.add(TRENCH_LEFT_OUT_APPROACH);
+        TRENCH_POSES.add(TRENCH_LEFT_IN_APPROACH);
     }
 
     public static boolean isRedSide() {
         if (DriverStation.getAlliance().isPresent()
-                && DriverStation.getAlliance().get() == DriverStation.Alliance.Red) {
-            return true;
+                && DriverStation.getAlliance().get() != DriverStation.Alliance.Red) {
+            return false;
         }
-        return false;
+        return true;
     }
 }
--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -30,6 +30,8 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -38,6 +40,7 @@ import frc.robot.Vision;
 import frc.robot.constants.TunerConstants;
 import frc.robot.constants.TunerConstants.TunerSwerveDrivetrain;
 import java.util.Optional;
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
@@ -52,7 +55,7 @@ import java.util.function.Supplier;
  */
 public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Subsystem {
   PIDController drivePID = new PIDController(DRIVE_P, DRIVE_I, DRIVE_D);
-  Pose2d[] trenchPoses;
+  Pose2d[] trenchPoses = LEFT_OUT;
   private static final double kSimLoopPeriod = 0.004; // 4 ms
   private Notifier m_simNotifier = null;
   private double m_lastSimTime;
@@ -334,32 +337,64 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         });
   }
 
-  public Command pidThroughTrench() {
-    FieldCentricFacingAngle request = new SwerveRequest.FieldCentricFacingAngle()
-        .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
-        .withDeadband(MAX_DRIVE_SPEED * 0.01)
-        .withRotationalDeadband(MAX_ROTATIONAL_SPEED * 0.01)
-        .withHeadingPID(ROTATION_P, ROTATION_I, ROTATION_D);
+  public BooleanSupplier whichTrench(Pose2d pose) {
+    return () -> {
+      return getState().Pose.nearest(TRENCH_POSES).equals(pose);
+    };
+  }
 
-    if (getState().Pose.nearest(TRENCH_POSES).equals(LEFT_OUT[0])) {
-      trenchPoses = LEFT_OUT;
-    } else if (getState().Pose.nearest(TRENCH_POSES).equals(LEFT_IN[0])) {
-      trenchPoses = LEFT_IN;
-    } else if (getState().Pose.nearest(TRENCH_POSES).equals(RIGHT_OUT[0])) {
-      trenchPoses = RIGHT_OUT;
-    } else if (getState().Pose.nearest(TRENCH_POSES).equals(RIGHT_IN[0])) {
-      trenchPoses = RIGHT_IN;
-    }
-
+  public Command doTrenches() {
+    double transition = 0.3;
     return new SequentialCommandGroup(
-        pidToPoint(trenchPoses[0])
-            .until(() -> getState().Pose.getTranslation().getDistance(trenchPoses[0].getTranslation()) < 0.25),
-        pidToPoint(trenchPoses[1])
-            .until(() -> getState().Pose.getTranslation().getDistance(trenchPoses[1].getTranslation()) < 0.1),
-        pidToPoint(trenchPoses[2])
-            .until(() -> getState().Pose.getTranslation().getDistance(trenchPoses[2].getTranslation()) < 0.1),
-        pidToPoint(trenchPoses[3])
-            .until(() -> getState().Pose.getTranslation().getDistance(trenchPoses[3].getTranslation()) < 0.25));
+        new ConditionalCommand(new SequentialCommandGroup(
+            pidToPoint(LEFT_OUT[0])
+                .until(() -> getState().Pose.getTranslation().getDistance(LEFT_OUT[0].getTranslation()) < transition),
+            pidToPoint(LEFT_OUT[1])
+                .until(() -> getState().Pose.getTranslation().getDistance(LEFT_OUT[1].getTranslation()) < transition),
+            pidToPoint(LEFT_OUT[2])
+                .until(() -> getState().Pose.getTranslation().getDistance(LEFT_OUT[2].getTranslation()) < transition),
+            pidToPoint(LEFT_OUT[3])
+                .until(() -> getState().Pose.getTranslation().getDistance(LEFT_OUT[3].getTranslation()) < transition)),
+            new InstantCommand(() -> {
+            }), whichTrench(LEFT_OUT[0])),
+        new ConditionalCommand(new SequentialCommandGroup(
+            pidToPoint(LEFT_IN[0])
+                .until(() -> getState().Pose.getTranslation().getDistance(LEFT_IN[0].getTranslation()) < transition),
+            pidToPoint(LEFT_IN[1])
+                .until(() -> getState().Pose.getTranslation().getDistance(LEFT_IN[1].getTranslation()) < transition),
+            pidToPoint(LEFT_IN[2])
+                .until(() -> getState().Pose.getTranslation().getDistance(LEFT_IN[2].getTranslation()) < transition),
+            pidToPoint(LEFT_IN[3])
+                .until(() -> getState().Pose.getTranslation().getDistance(LEFT_IN[3].getTranslation()) < transition)),
+            new InstantCommand(() -> {
+            }), whichTrench(LEFT_IN[0])),
+        new ConditionalCommand(
+            new SequentialCommandGroup(
+                pidToPoint(RIGHT_OUT[0])
+                    .until(
+                        () -> getState().Pose.getTranslation().getDistance(RIGHT_OUT[0].getTranslation()) < transition),
+                pidToPoint(RIGHT_OUT[1])
+                    .until(
+                        () -> getState().Pose.getTranslation().getDistance(RIGHT_OUT[1].getTranslation()) < transition),
+                pidToPoint(RIGHT_OUT[2])
+                    .until(
+                        () -> getState().Pose.getTranslation().getDistance(RIGHT_OUT[2].getTranslation()) < transition),
+                pidToPoint(RIGHT_OUT[3])
+                    .until(() -> getState().Pose.getTranslation()
+                        .getDistance(RIGHT_OUT[3].getTranslation()) < transition)),
+            new InstantCommand(() -> {
+            }), whichTrench(RIGHT_OUT[0])),
+        new ConditionalCommand(new SequentialCommandGroup(
+            pidToPoint(RIGHT_IN[0])
+                .until(() -> getState().Pose.getTranslation().getDistance(RIGHT_IN[0].getTranslation()) < transition),
+            pidToPoint(RIGHT_IN[1])
+                .until(() -> getState().Pose.getTranslation().getDistance(RIGHT_IN[1].getTranslation()) < transition),
+            pidToPoint(RIGHT_IN[2])
+                .until(() -> getState().Pose.getTranslation().getDistance(RIGHT_IN[2].getTranslation()) < transition),
+            pidToPoint(RIGHT_IN[3])
+                .until(() -> getState().Pose.getTranslation().getDistance(RIGHT_IN[3].getTranslation()) < transition)),
+            new InstantCommand(() -> {
+            }), whichTrench(RIGHT_IN[0])));
   }
 
   /**
@@ -406,9 +441,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     for (Vision camera : cameras) {
       camera.periodic();
     }
-
-    DogLog.log("Trenches", trenchPoses);
-
     // not mine
     /*
      * Periodically try to apply the operator perspective.
