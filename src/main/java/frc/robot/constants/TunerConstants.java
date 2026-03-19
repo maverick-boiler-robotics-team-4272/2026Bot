ab802573e49108b4dcd34db7da5c46f173c995a7
DrRobbie <menon.elliott@gmail.com>
2026-03-19 12:13:53 -0400

    Fix rotations from swapped modules

--- src/main/java/frc/robot/constants/TunerConstants.java
+++ src/main/java/frc/robot/constants/TunerConstants.java
@@ -147,7 +147,7 @@ public class TunerConstants {
     private static final int kFrontRightDriveMotorId = 1;
     private static final int kFrontRightSteerMotorId = 11;
     private static final int kFrontRightEncoderId = 21;
-    private static final Angle kFrontRightEncoderOffset = Rotations.of(-0.112549);
+    private static final Angle kFrontRightEncoderOffset = Rotations.of(-0.002685546875);
     private static final boolean kFrontRightSteerMotorInverted = false;
     private static final boolean kFrontRightEncoderInverted = false;
 
@@ -158,7 +158,7 @@ public class TunerConstants {
     private static final int kBackLeftDriveMotorId = 3;
     private static final int kBackLeftSteerMotorId = 13;
     private static final int kBackLeftEncoderId = 23;
-    private static final Angle kBackLeftEncoderOffset = Rotations.of(0.243896484375);
+    private static final Angle kBackLeftEncoderOffset = Rotations.of(0.24365234375);
     private static final boolean kBackLeftSteerMotorInverted = false;
     private static final boolean kBackLeftEncoderInverted = false;
 
@@ -169,7 +169,7 @@ public class TunerConstants {
     private static final int kBackRightDriveMotorId = 4;
     private static final int kBackRightSteerMotorId = 14;
     private static final int kBackRightEncoderId = 24;
-    private static final Angle kBackRightEncoderOffset = Rotations.of(-0.197265625);
+    private static final Angle kBackRightEncoderOffset = Rotations.of(0.175537109375);
     private static final boolean kBackRightSteerMotorInverted = false;
     private static final boolean kBackRightEncoderInverted = false;
 
