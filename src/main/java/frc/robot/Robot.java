cfd505ac005e6cf9c27dcc26ae3fc516d58737f1
DrRobbie <menon.elliott@gmail.com>
2026-03-21 13:09:22 -0400

    Add some game data methods

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.utils.elastic.Elastic;
+import frc.robot.utils.gameData.GameData;
 import frc.robot.utils.periodic.PeriodicalUtil;
 
 public class Robot extends TimedRobot {
@@ -61,6 +62,7 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    GameData.setGameData();
   }
 
   @Override
