3c99b576cbff946d276cb51e3af50258c60bd95c
DrRobbie <menon.elliott@gmail.com>
2026-04-11 08:24:22 -0400

    Lower Hopper Speed

--- src/main/java/frc/robot/constants/SubsystemConstants.java
+++ src/main/java/frc/robot/constants/SubsystemConstants.java
@@ -20,7 +20,7 @@ public class SubsystemConstants {
     public static final String HOPPER_KEY = "Subsystems/Hopper/";
 
     public static final int HOPPER_LOWER_SPEED = 50;
-    public static final double HOPPER_UPPER_SPEED = -20.0;
+    public static final double HOPPER_UPPER_SPEED = -50.0;
   }
 
   public static class LoaderConstants {
