630fd13fe731e493cafacfb1df8c11178102ccba
DrRobbie <menon.elliott@gmail.com>
2026-04-13 20:32:26 -0400

    run at slower loop time

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,13 +18,15 @@ public class Robot extends TimedRobot {
   private final RobotContainer m_robotContainer;
 
   public Robot() {
-    // super(0.02);
+    super(0.03);
     m_robotContainer = new RobotContainer();
     Elastic.selectTab("Autonomous");
+    DogLog.setEnabled(true);
 
     DogLog.setOptions(
-        new DogLogOptions().withLogEntryQueueCapacity(100));
-    RobotController.setBrownoutVoltage(6.25);
+        new DogLogOptions().withNtPublish(true));
+    RobotController.setBrownoutVoltage(6);
+    DriverStation.silenceJoystickConnectionWarning(true);
 
   }
 
